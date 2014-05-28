b978adafb3740900dc7794a39cd4f8a3ca03239e
Tiffany Tse <ttse@coldfrontlabs.ca>
2014-05-28 10:32:29 -0400

    Added services images, styles and new text, Fixes #1

--- services-bar.php
+++ services-bar.php
@@ -10,27 +10,27 @@
 <div class="service-bar">
 <aside>
 <a class="service-type" title="Public" href="<?php get_template_directory_uri(); ?>public/">
-	<img src="<?php echo get_theme_root_uri() ?>/castle-glenn/imgs/institutional.jpg">
+	<img src="<?php echo get_theme_root_uri() ?>/castle-glenn/imgs/public.jpg">
 	<h2>Public</h2>
 </a>
-	<p>Description of type.</p>
+	<p>Municipal, Provincial and Federal governments.</p>
 </aside>
 
 <aside>
 <a class="service-type middle" title="Private" href="<?php get_template_directory_uri(); ?>private/">
-<img src="<?php echo get_theme_root_uri() ?>/castle-glenn/imgs/business.jpg">
+<img src="<?php echo get_theme_root_uri() ?>/castle-glenn/imgs/private.jpg">
 <h2>Private</h2>
 </a>
-<p>Description of type.</p>
+<p>Residential and commercial developers, the resource extraction sector, etc.</p>
 </aside>
 
 
 <aside>
 <a class="service-type" title="Institutional" href="<?php get_template_directory_uri(); ?>institutional/">
-<img src="<?php echo get_theme_root_uri() ?>/castle-glenn/imgs/government.jpg">
+<img src="<?php echo get_theme_root_uri() ?>/castle-glenn/imgs/institutional.jpg">
 <h2>Institutional</h2>
 </a>
-<p>Description of type.</p>
+<p>Hospitals, schools, retirement homes, etc.</p>
 </aside>
 
 </div>
